dd5738a66ff26c8bec4dfc36451073a1fb6b6bff
Jonathan.Hughes@skycity.co.nz <Jonathan.Hughes@skycity.co.nz>
2018-03-22 10:55:41 +1300

    Test file added

---
+++ git_commit.cmd
@@ -0,0 +1,13 @@
+echo off
+set projectName=%1
+set commitDesc=%2
+set projectUrl=https://github.com/Groovejets2/%projectName%.git
+
+git add .
+
+
+git rm git_commit.cmd
+
+git commit -m %commitDesc%
+
+git push origin master
